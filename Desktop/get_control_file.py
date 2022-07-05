a11a5cf3e1ee688e288ccb8ddd2e71a9e3500807
George Maynard <george.maynard@noaa.gov>
2022-07-05 10:36:30 -0400

    running on test RPi

---
+++ Desktop/get_control_file.py
@@ -0,0 +1,25 @@
+## This script downloads the control file from the eMOLT development API
+
+import requests
+
+## Prompt the user for a vessel name
+vesselname=input("Please enter vessel name: ")
+
+## Standardize the vessel name
+vesselname=vesselname.replace(" ","_")
+vesselname=vesselname.replace("F/V","")
+
+## Read in the API connection info
+with open ("/home/pi/Documents/config.yml","r") as yamlfile:
+  API_config=yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+address = ['default']['IP']
+
+## Form the URL
+url = 'http://'+address+'/getControl_File?vessel='+vesselname
+
+## Request the control file
+r = requests.get(url, allow_redirects=True)
+
+## Save the control file to the appropriate location
+open('/home/pi/Desktop/setup_rtd.py', 'wb').write(r.content)
